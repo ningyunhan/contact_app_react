caeebe4c40611ecfcbd7aa16ee2ff1585ab21e96
Yunhan Ning <yunhanning@Yunhans-MacBook-Pro.local>
2017-12-06 00:25:28 -0500

    change to new repo

---
+++ front_end/src/ListContacts.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+//const imgUrl="http://coding-robin.de/images/pictures/opencv_positive_cropped_scaled_01.jpg"
+
+class ListContacts extends Component{
+	render(){
+		//console.log(typeof(this.props.contacts[0].avatarURL))
+		return(
+			<ol className='contact-list'>
+				{this.props.contacts.map((contact) => (
+					<li key={contact.id} className='contact-list-item'>
+						<div className='contact-avatar' style={{
+							backgroundImage: 'url('+ contact.avatarURL +')'
+						}}></div>
+						<div className='contact-details'>
+							<p>{contact.name}</p>
+							<p>{contact.email}</p>
+						</div>
+						<button className='contact-remove'>
+							Remove
+						</button>
+					</li>
+				))}
+			</ol>
+		)
+	}
+}
+
+export default ListContacts
