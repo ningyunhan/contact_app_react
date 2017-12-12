a58b3dfbda0d3a04c59848315213ca9c80d95489
ningyunhan <ningyunhan19940829@gmail.com>
2017-12-12 02:20:15 -0500

    add search bar unfinished

--- front_end/src/ListContacts.js
+++ front_end/src/ListContacts.js
@@ -2,25 +2,45 @@ import React, {Component} from 'react'
 //const imgUrl="http://coding-robin.de/images/pictures/opencv_positive_cropped_scaled_01.jpg"
 import PropTypes from  'prop-types'
 class ListContacts extends Component{
+
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({query: query.trim()})
+	}
+
 	render(){
 		//console.log(typeof(this.props.contacts[0].avatarURL))
 		return(
-			<ol className='contact-list'>
-				{this.props.contacts.map((contact) => (
-					<li key={contact.id} className='contact-list-item'>
-						<div className='contact-avatar' style={{
-							backgroundImage: 'url('+ contact.avatarURL +')'
-						}}></div>
-						<div className='contact-details'>
-							<p>{contact.name}</p>
-							<p>{contact.email}</p>
-						</div>
-						<button onClick={() => (this.props.onDeleteContact(contact))} className='contact-remove'>
-							Remove
-						</button>
-					</li>
-				))}
-			</ol>
+			<div className='list-contacts'>
+				<div className='list-contacts-top'>
+					<input
+						className='search-contacts'
+						type='text'
+						placeholder='Search contacts'
+						value={this.state.query}
+						onChange={(event) => this.updateQuery(event.target.value)}
+					/>
+				</div>
+				<ol className='contact-list'>
+					{this.props.contacts.map((contact) => (
+						<li key={contact.id} className='contact-list-item'>
+							<div className='contact-avatar' style={{
+								backgroundImage: 'url('+ contact.avatarURL +')'
+							}}></div>
+							<div className='contact-details'>
+								<p>{contact.name}</p>
+								<p>{contact.email}</p>
+							</div>
+							<button onClick={() => (this.props.onDeleteContact(contact))} className='contact-remove'>
+								Remove
+							</button>
+						</li>
+					))}
+				</ol>			
+			</div>
 		)
 	}
 }
